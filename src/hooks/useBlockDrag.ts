e2d8576d9af8fa8ac4695e3de354c1acc18c4907
张国喜 <2458734304@qq.com>
2022-05-18 08:18:03 +0800

    节点拖拽辅助线03

--- src/hooks/useBlockDrag.ts
+++ src/hooks/useBlockDrag.ts
@@ -29,6 +29,8 @@ export default function useBlockDrag(data: Ref<TotalData>, markLineRef) {
         moveStart = true;
         dragState.startX = event.clientX;
         dragState.startY = event.clientY;
+        dragState.startLeft = focusList.value[0].left;
+        dragState.startTop = focusList.value[0].top;
         dragState.startPos = focusList.value.map(({ top, left }) => ({
             top,
             left,
@@ -70,21 +72,59 @@ export default function useBlockDrag(data: Ref<TotalData>, markLineRef) {
             }); // B右对A右
 
             //TODO Y 垂直方向移动
+            dragState.lines.y.push({ showTop: top, top: top }); // B顶对A顶
+            dragState.lines.y.push({
+                showTop: top + height! / 2,
+                top: top + height! / 2,
+            }); // B中对A顶
+            dragState.lines.y.push({
+                showTop: top + height!,
+                top: top + height!,
+            }); // B低对A顶
+
+            dragState.lines.y.push({ showTop: top, top: top - aHeight! / 2 }); // B顶对A中
+            dragState.lines.y.push({
+                showTop: top + height! / 2,
+                top: top + height! / 2 - aHeight! / 2,
+            }); // B中对A中
+            dragState.lines.y.push({
+                showTop: top + height!,
+                top: top + height! - aHeight! / 2,
+            }); // B低对A中
+
+            dragState.lines.y.push({ showTop: top, top: top - aHeight! }); // B顶对A底
+            dragState.lines.y.push({
+                showTop: top + height! / 2,
+                top: top + height! / 2 - aHeight!,
+            }); // B中对A底
+            dragState.lines.y.push({
+                showTop: top + height!,
+                top: top + height! - aHeight!,
+            }); // B低对A底
         });
     }
     function onMousemoveInDocument(e: MouseEvent) {
-        const { clientX, clientY } = e;
-        const deviation = 10;
-        const block = focusList.value[0];
+        let { clientX, clientY } = e;
+        const deviation = 5;
+        const left = clientX - dragState.startX + dragState.startLeft;
+        const top = clientY - dragState.startY + dragState.startTop;
+        markLineRef.value.hideXMarkLine();
         markLineRef.value.hideYMarkLine();
-        console.log(dragState.lines.x);
         for (const x of dragState.lines.x) {
-            const durX = block.left - x.left;
-            console.log('durX', durX);
-            if (Math.abs(durX) < deviation) {
+            if (Math.abs(left - x.left) < deviation) {
                 markLineRef.value.showYMarkLine({
                     left: x.showLeft,
                 });
+                clientX = x.left + dragState.startX - dragState.startLeft;
+                break;
+            }
+        }
+        for (const y of dragState.lines.y) {
+            if (Math.abs(top - y.top) < deviation) {
+                markLineRef.value.showXMarkLine({
+                    top: y.showTop,
+                });
+                clientY = y.top + dragState.startY - dragState.startTop;
                 break;
             }
         }
